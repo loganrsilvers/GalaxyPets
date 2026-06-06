e9bed8d9ce946bb4922536a1e3191914d92f937d
Logan Silvers <loganrsilvers@gmail.com>
2026-06-06 15:16:35 -0500

    assembly info

--- src/obj/Debug/net10.0/GalaxyPets.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/GalaxyPets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalaxyPets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74031e2bd502563152ee4e6a81f6ccd50569a5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da257fb7e12e20e6d994c489a799b081802fcf58")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalaxyPets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalaxyPets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
